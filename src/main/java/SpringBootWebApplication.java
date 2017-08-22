37325acf668f63f36a4d92292ec810f35b5cb398
Wala86 <benamorwala@yahoo.fr>
2017-08-22 15:40:11 +0200

    Update SpringBootWebApplication.java

--- src/main/java/SpringBootWebApplication.java
+++ src/main/java/SpringBootWebApplication.java
@@ -1,12 +1,18 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "controller" })
+@EntityScan(basePackages = { "domain" })
+@EnableJpaRepositories
+@EnableConfigurationProperties
 public class SpringBootWebApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootWebApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+	}
 }
