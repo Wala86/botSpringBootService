a5e497be5e48ba8459ef836c5eb28d2a0e9244cd
Wala86 <benamorwala@yahoo.fr>
2017-08-22 16:35:27 +0200

    Update SecurityConfiguration.java

--- src/main/java/configuration/SecurityConfiguration.java
+++ src/main/java/configuration/SecurityConfiguration.java
@@ -1,13 +1,11 @@
 package configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration  {
 
-    @Override
+ /*   @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
                 .authorizeRequests().antMatchers("/console/**").permitAll();
@@ -15,5 +13,5 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         httpSecurity.csrf().disable();
         httpSecurity.headers().frameOptions().disable();
     }
-
+*/
 }
