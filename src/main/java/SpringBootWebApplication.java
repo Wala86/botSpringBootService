5d80983c0e722ecada7a102f73a65c71f0304a2c
Wala86 <benamorwala@yahoo.fr>
2017-08-22 15:05:39 +0200

    Update SpringBootWebApplication.java

--- src/main/java/SpringBootWebApplication.java
+++ src/main/java/SpringBootWebApplication.java
@@ -1,5 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "controller" })
