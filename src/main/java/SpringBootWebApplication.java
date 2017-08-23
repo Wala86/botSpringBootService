e29a4a4b9d5cf5492d8fc967844fa4ccd85f2db8
Wala86 <benamorwala@yahoo.fr>
2017-08-23 17:28:49 +0200

    Update SpringBootWebApplication.java

--- src/main/java/SpringBootWebApplication.java
+++ src/main/java/SpringBootWebApplication.java
@@ -7,22 +7,13 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import com.linecorp.bot.model.event.Event;
-import com.linecorp.bot.model.event.MessageEvent;
-import com.linecorp.bot.model.event.message.TextMessageContent;
-import com.linecorp.bot.model.message.TextMessage;
-import com.linecorp.bot.spring.boot.annotation.EventMapping;
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
-
 import domain.Product;
 import repositories.ProductRepository;
 
 @SpringBootApplication
-@LineMessageHandler
-
 @ComponentScan(basePackages = { "controller" })
 @EntityScan(basePackages = { "domain" })
-@EnableJpaRepositories(basePackages = { "repositories" })
+@EnableJpaRepositories (basePackages = { "repositories" })
 @EnableConfigurationProperties
 public class SpringBootWebApplication implements CommandLineRunner {
 	@Autowired
@@ -41,16 +32,5 @@ public class SpringBootWebApplication implements CommandLineRunner {
 		prod.setPrice(500);
 		rep.save(prod);
 
-	}
-
-	@EventMapping
-	public TextMessage handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
-		System.out.println("event: " + event);
-		return new TextMessage(event.getMessage().getText());
-	}
-
-	@EventMapping
-	public void handleDefaultMessageEvent(Event event) {
-		System.out.println("event: " + event);
 	}
 }
