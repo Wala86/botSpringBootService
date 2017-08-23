fe527332cb3120762a3d9cfb24594bc161529483
Wala86 <benamorwala@yahoo.fr>
2017-08-23 16:43:45 +0200

    Update BotController.java

--- src/main/java/controller/BotController.java
+++ src/main/java/controller/BotController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -13,8 +14,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.linecorp.bot.client.LineMessagingServiceBuilder;
+import com.linecorp.bot.model.ReplyMessage;
+import com.linecorp.bot.model.message.ImageMessage;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.response.BotApiResponse;
+
 import domain.Product;
 import repositories.ProductRepository;
+import retrofit2.Response;
 
 @RestController
 public class BotController {
@@ -31,7 +39,8 @@ public class BotController {
 	}
 
 	@RequestMapping(value = "/webhook", method = RequestMethod.POST)
-	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> map) throws JSONException {
+	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> map)
+			throws JSONException, IOException {
 		System.out.println("**********webhook/***************" + map);
 		System.out.println("******************************");
 		for (Map.Entry<String, Object> entry : map.entrySet()) {
@@ -64,6 +73,14 @@ public class BotController {
 				"https://i.pinimg.com/736x/96/a0/54/96a0544ab7b6fa7cbdddff9c5d8397be--japanese-hairstyles-korean-hairstyles.jpg");
 
 		System.out.println("************* ******************" + map.get("result"));
+		ImageMessage textMessage = new ImageMessage(
+				"https://i.pinimg.com/736x/96/a0/54/96a0544ab7b6fa7cbdddff9c5d8397be--japanese-hairstyles-korean-hairstyles.jpg",
+				"https://i.pinimg.com/736x/96/a0/54/96a0544ab7b6fa7cbdddff9c5d8397be--japanese-hairstyles-korean-hairstyles.jpg");
+		ReplyMessage replyMessage = new ReplyMessage("415707c0f42b4bc5a411bcc197789d5e", textMessage);
+		Response<BotApiResponse> response = LineMessagingServiceBuilder.create(
+				"mmud/Cez+bvYykKzBnemzXm6fAXOPg6s9SEYD52jcBdCeFM/sxyIJxQaz9xpC0i2fW73wibxwtkHH45DNy6f9M8wj5GYAYxNf4NOZo0kfI68PmQzlbqqCQrg4C89zAtSlpp6YtH8/EJGk5MWZUTtbQdB04t89/1O/w1cDnyilFU=")
+				.build().replyMessage(replyMessage).execute();
+		System.out.println(response.code() + " " + response.message());
 
 		return json;
 
